cb2ac8e28606359b4041b98a2b45cc4316aa9bf6
tonky0110 <tonky0110@gmail.com>
2022-01-31 22:08:09 +0900

    2.0 Dev Server - 걸프 웹서버 추가 및 proxy설정

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
   pug: {
@@ -14,8 +15,25 @@ const pug = () =>
 
 const clean = () => del(["build"]);
 
+// gulp-webserver 프록시 설정 - https://stackoverflow.com/questions/31047493/using-gulp-webserver-with-proxy-and-middleware/32259691
+const webserver = () =>
+  gulp.src("build").pipe(
+    ws({
+      port: 3000,
+      livereload: true,
+      open: "http://localhost:3000",
+      // proxies: [
+      //     {
+      //         source:'/api', target: 'http://********',
+      //     }
+      // ]
+    })
+  );
+
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.series([webserver]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
